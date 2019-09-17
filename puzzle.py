161e1c68ebd0cab9c9d70755d340115633f57f32
Alison Muñoz Capote <alison.capote1982@gmail.com>
2019-09-17 18:35:27 -0500

    heuristic method implemented

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,22 @@
 #Main class
 
+from bestFS import Node
+
+FINAL_STATE = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+
 class Puzzle:
-    def __init__(self, initialState, finalState):
-        self.initialState = initialState
-        self.finalState   = finalState
+    def __init__(self, initialState):
+        self.initialState = initialState        
+    
+    def _heuristic(self, state):                
+        i = 0
+        count = 0
+        while (i < 9): #Count the wrong positions
+            x = i % 3 #To get x coordinates
+            y = i // 3 #To get y coordinates
+            if state.squareDistribution[x][y] != FINAL_STATE[x][y]:
+                count += 1
+            i +=1
+        state.cost = state.high + count
+    
+    
