d54a33e18e4812032e7a1335e24e2fe1fc626cdc
Alison Muñoz Capote <alison.capote1982@gmail.com>
2019-09-18 13:32:40 -0500

    Update searchForSolution method and create the hashTable class

--- FSOrderedList.py
+++ FSOrderedList.py
@@ -1,8 +1,32 @@
 #Ordered List class
 
-class FSOrderedList:
-    def __init__(self):        
-        self.list = []
+class HashTable:
+    def __init__(self):
+        self.table = [None] * 127
     
-    def add(self, item):
-        self.list.append(item)
+    # Función hash
+    def Hash_func(self, value):
+        key = 0
+        for i in range(0,len(value)):
+            key += ord(value[i])
+        return key % 127
+
+    def Insert(self, value): # Metodo para ingresar elementos
+        hash = self.Hash_func(value)
+        if self.table[hash] is None:
+            self.table[hash] = value
+   
+    def Search(self,value): # Metodo para buscar elementos
+        hash = self.Hash_func(value);
+        if self.table[hash] is None:
+            return None
+        else:
+            return hex(id(self.table[hash]))
+  
+    def Remove(self,value): # Metodo para eleminar elementos
+        hash = self.Hash_func(value);
+        if self.table[hash] is None:
+            print("No hay elementos con ese valor", value)
+        else:
+            print("Elemento con valor", value, "eliminado")
+            self.table[hash] is None;
