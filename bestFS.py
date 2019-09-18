47334f53dc759145d1fd4e39ae83c83f63aa5c5e
unknown <danisetg@gmail.com>
2019-09-18 15:13:21 -0500

    fixed movement directions

--- bestFS.py
+++ bestFS.py
@@ -8,32 +8,32 @@ class Node:
         self.emptySpacePositionX = emptySpacePositionX
         self.emptySpacePositionY = emptySpacePositionY
     
-    def getUpDistribution(self):
-        if self.emptySpacePositionY == 0: #Cant move up 
+    def getLeftDistribution(self):
+        if self.emptySpacePositionY == 0: #Cant move left 
             return None
         squareDistribution = self.squareDistribution
         squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY] = self.squareDistribution[self.emptySpacePositionX - 1][self.emptySpacePositionY] 
         squareDistribution[self.emptySpacePositionX - 1][self.emptySpacePositionY] = -1
         return squareDistribution
     
-    def getDownDistribution(self):
-        if self.emptySpacePositionY == 2: #Cant move down
+    def getRightDistribution(self):
+        if self.emptySpacePositionY == 2: #Cant move right
             return None
         squareDistribution = self.squareDistribution
         squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY] = self.squareDistribution[self.emptySpacePositionX + 1][self.emptySpacePositionY] 
         squareDistribution[self.emptySpacePositionX + 1][self.emptySpacePositionY] = -1
         return squareDistribution
     
-    def getLeftDistribution(self):
-        if self.emptySpacePositionX == 0: #Cant move left
+    def getUptDistribution(self):
+        if self.emptySpacePositionX == 0: #Cant move up
             return None
         squareDistribution = self.squareDistribution
         squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY] = self.squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY-1] 
         squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY - 1]  = -1
         return squareDistribution
     
-    def getRightDistribution(self):
-        if self.emptySpacePositionX == 2: #Cant move right
+    def getDownDistribution(self):
+        if self.emptySpacePositionX == 2: #Cant move down
             return None
         squareDistribution = self.squareDistribution
         squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY] = self.squareDistribution[self.emptySpacePositionX][self.emptySpacePositionY + 1] 
