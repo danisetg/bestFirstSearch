0b5b59f6a5d3121eb056540cde6ee2f30ddc5c08
Alison Muñoz Capote <alison.capote1982@gmail.com>
2019-09-19 20:12:43 -0500

    Create  _printState function

--- puzzle.py
+++ puzzle.py
@@ -24,7 +24,7 @@ class Puzzle:
 
     def _searchForSolution(self):
         while (not self.newStates.empty()) and (self.solution is None): #Check if the new states queue is empty (Q ≠ Ø) and (P ∩ Q = Ø) who represent the not found solution
-            state = self.newStates.get()  #Get the better state
+            state = self.newStates.get()  #Get the better state            
             self.visitedStates.Insert(state.toString()) #Put the state to the visited list (Consult if this is the correct place (*)) 
             if state.squareDistribution == self.finalState: #Base condition (P ∩ Q = Ø)
                 self.solution = state
@@ -45,45 +45,53 @@ class Puzzle:
         state.cost = state.high + count
     
     def _getPath(self, list):        
-        while not(self.solution.father is None):
+        list.append('Done')        
+        while not(self.solution.father is None):                        
             row =  self.solution.emptySpacePositionRow - self.solution.father.emptySpacePositionRow # Horizontal move
             column =  self.solution.emptySpacePositionColumn - self.solution.father.emptySpacePositionColumn # Vertical move
             if column != 0:  # Left or right move
                 if column < 0:
                     list.append('Left')
                 else:
-                    list.append('Rigth')
+                    list.append('Right')
             else: # Up or down move
                 if row < 0:
                     list.append('Up')
                 else:
-                    list.append('Down')
-            self.solution = self.solution.father        
+                    list.append('Down')                                
+            self.solution = self.solution.father                    
     
     def _expand(self, state):
         #Try to create the childs
         #Remember we must check if the new child exist in the visited states!!!! (*)
-        #Up move
+        #Up move        
         if not(state.getUpDistribution() is None):
             up = Node(state.getUpDistribution(), state, state.high + 1, state.emptySpacePositionRow - 1, state.emptySpacePositionColumn)
             self._heuristic(up) #Update cost
-            if self.visitedStates.Search(up.toString()) is None:
+            if self.visitedStates.Search(up.toString()) is None:                
                 self.newStates.put(up, up.cost) #Put the new child state to the states queue
         #Down move    
         if not(state.getDownDistribution() is None):
             down = Node(state.getDownDistribution(), state, state.high + 1, state.emptySpacePositionRow + 1, state.emptySpacePositionColumn)
             self._heuristic(down) #Update cost
-            if self.visitedStates.Search(down.toString()) is None:
+            if self.visitedStates.Search(down.toString()) is None:                
                 self.newStates.put(down, down.cost) #Put the new child state to the states queue
         #Left move    
         if not(state.getLeftDistribution() is None):
             left = Node(state.getLeftDistribution(), state, state.high + 1, state.emptySpacePositionRow, state.emptySpacePositionColumn - 1)
             self._heuristic(left) #Update cost
-            if self.visitedStates.Search(left.toString()) is None:
+            if self.visitedStates.Search(left.toString()) is None:                
                 self.newStates.put(left, left.cost) #Put the new child state to the states queue
         #Right move
         if not(state.getRightDistribution() is None):
             right = Node(state.getRightDistribution(), state, state.high + 1, state.emptySpacePositionRow, state.emptySpacePositionColumn + 1)
             self._heuristic(right) #Update cost
-            if self.visitedStates.Search(right.toString()) is None:
+            if self.visitedStates.Search(right.toString()) is None:                
                 self.newStates.put(right, right.cost) #Put the new child state to the states queue
+    
+    def _printState(self, distribution):        
+        for i in range(len(distribution[0])):
+            for j in range(len(distribution)):
+                print(distribution[j][i], end="")
+            print("") 
+        print("------")
